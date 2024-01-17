4b81892677cc8be625e24e7b0c82a8a66da2ef1f
Shashank Tiwari <133049320+shashankkk23@users.noreply.github.com>
2024-01-17 12:46:50 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -20,15 +20,15 @@ async function checkWeather(city) {
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
   
     if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = " images/clouds.png";
+      weatherIcon.src = " clouds.png";
     } else if (data.weather[0].main == "Clear") {
-      weatherIcon.src = " images/clear.png";
+      weatherIcon.src = " clear.png";
     } else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = " images/rain.png";
+      weatherIcon.src = " rain.png";
     } else if (data.weather[0].main == "Drizle") {
-      weatherIcon.src = " images/drizle.png";
+      weatherIcon.src = " drizle.png";
     } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = " images/mist.png";
+      weatherIcon.src = " mist.png";
     }
   
     document.querySelector(".weather").style.display = "block"
